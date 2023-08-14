8853e8fb02e72e546ea42bc1f77c4a529201a4dc
Mr-Dickson <dickson.ankamah8@gmail.com>
2023-08-13 19:06:48 -0700

    Add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - entry
+*
+* Return: zero
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
