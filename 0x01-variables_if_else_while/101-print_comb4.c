2da550336857b3493a57cde1aaab338ec326f7cd
Mr-Dickson <dickson.ankamah8@gmail.com>
2023-08-13 19:45:30 -0700

    Add advanced task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+it main(void)
+{
+it n, t, z;
+
+for (z = '0'; z <= '7'; z++)
+{
+for (n = z + 1; n <= '9'; n++)
+{
+for (t = n + 1; t <= '9'; t++)
+{
+putchar(z);
+putchar(n);
+putchar(t);
+if (z < '7' || n < '8' || t < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
