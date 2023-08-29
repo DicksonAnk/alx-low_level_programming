2408fd048c2530b720aba620139f48fd235f63af
Mr-Dickson <dickson.ankamah8@gmail.com>
2023-08-29 11:29:21 -0700

    Add task 5 and 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - returns ther absolute of the value passed
+*@value: value to convert to its absolute
+*
+* Return: returns zero
+*/
+
+int _abs(int value)
+{
+	return (abs(value));
+}
